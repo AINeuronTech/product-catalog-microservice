0da4f3a62460984332a5d4881edc133eae639a90
mrahman <rahmanww@gmail.com>
2021-05-26 23:30:33 -0400

    hystrix

--- src/main/java/com/commerce/productcatalogservices/rest/ProductCatalogResources.java
+++ src/main/java/com/commerce/productcatalogservices/rest/ProductCatalogResources.java
@@ -3,12 +3,14 @@ package com.commerce.productcatalogservices.rest;
 import com.commerce.productcatalogservices.model.Product;
 import com.commerce.productcatalogservices.model.ProductCatalog;
 import com.commerce.productcatalogservices.model.UserReview;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +22,7 @@ public class ProductCatalogResources {
     public RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<ProductCatalog> getProductCatalog(@PathVariable("userId")String userId){
 
         UserReview userRating = restTemplate.getForObject("http://product-review-service/reviews-data/user/"+userId, UserReview.class);
@@ -30,4 +33,11 @@ public class ProductCatalogResources {
         }).collect(Collectors.toList());
 
     }
+
+
+    @SuppressWarnings("unused")
+    public List<ProductCatalog> getFallbackCatalog(@PathVariable("userId")String userId){
+        return Arrays.asList(new ProductCatalog("No Product Itrmes are found","",0));
+    }
+
 }
